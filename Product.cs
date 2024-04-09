8108e13739efb635be1014fe888f06c18c587c4b
Mayank5054 <mayanksheladiya49@gmail.com>
2024-04-09 10:45:28 +0530

    bug resolved :- New Category Created every time Product category changes

--- Product.cs
+++ Product.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ECommerce.Models
 {
     public class Product
@@ -22,5 +23,14 @@ namespace ECommerce.Models
         [Display(Name = "Selling Price")]
         [Range(1,1000)]
         public double Price { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category category { get; set; }
+
+        public ProductWithName Select(Func<object, ProductWithName> value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ ProductWithName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Models
+{
+    public class ProductWithName
+    {
+     
+        public int BookId { get; set; }
+  
+        public string BookTitle { get; set; }
+   
+        public string Description { get; set; }
+      
+        public string ISBN { get; set; }
+    
+        public string Author { get; set; }
+   
+    
+        public double Price { get; set; }
+
+        public string CategoryName { get; set; }
+     
+        
+    }
+}
